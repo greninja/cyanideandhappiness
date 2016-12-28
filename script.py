bc98cedcdca0ae90d54cc9f920825a1d95fc169f
greninja <sshadab84@gmail.com>
2016-12-28 22:45:13 +0530

    minor changes

--- script.py
+++ script.py
@@ -1,34 +1,42 @@
 import os
-import urllib
-import urlparse
-import requests
-
-from bs4 import BeautifulSoup
-
-
+try:
+	import urllib,urlparse,requests,multiprocessing
+	from bs4 import BeautifulSoup
+except ImportError:
+	raise ImportError("Module not installed")
 
 
-def main(i,j):
-	print(" The pics will be stored in CyanideNhappiness directory")
+
+def download_comic():
+	print(" The pics will be stored in CyanideNhappiness directory  \n")
 	
 	os.mkdir('CyanideNhappiness')
 	os.chdir('./CyanideNhappiness/')
-		
-	while (i <= 4409) :
+	i = 15
+	while (str(i) != 'latest') :
 		
 			
 		url = "http://explosm.net/comics/" + str(i)
 		
-		
-		
 		var = requests.get(url).text
+		
 		soup = BeautifulSoup(var,"html.parser")
 		
-		final_url = "https://" + soup.find("img",{"id":"main-comic"})['src'].strip("//") 
-		u = urlparse.urlparse(final_url).path.split('/')[-1]
-		urllib.urlretrieve(final_url, u)
-		j += 1
+		try:
+			final_url = "https://" + soup.find("img",{"id":"main-comic"})['src'].strip("//") 
+			u = urlparse.urlparse(final_url).path.split('/')[-1]
+			urllib.urlretrieve(final_url, u)
+		
+		except TypeError as e:
+			print "Comic not found"
+			
 		i += 1
 
-main(4400,1)
 
+
+if __name__ == "__main__":
+	
+	download_comic()
+
+
+#Todo: check if 'latest' referrer header is specified in BeautifulSoup object.
